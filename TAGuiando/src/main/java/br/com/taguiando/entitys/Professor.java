1da16a2cfb5f016fc0278db82bd0db51a52f3b5c
renatadegino <renatadegino@gmail.com>
2015-05-29 17:08:32 -0300

    at

---
+++ TAGuiando/src/main/java/br/com/taguiando/entitys/Professor.java
@@ -0,0 +1,21 @@
+package br.com.taguiando.entitys;
+
+public class Professor {
+
+	public String nome;
+	public int codigo;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	
+}
---
+++ TAGuiando/src/main/java/br/com/taguiando/entitys/Tag.java
@@ -0,0 +1,25 @@
+package br.com.taguiando.entitys;
+
+public class Tag {
+	
+	public int codigo;
+	public String nome;
+	public TipoTag tipo;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	
+
+}
