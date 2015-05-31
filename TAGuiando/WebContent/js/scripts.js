f6f4db5683fb3f7f99ec52936b020db3ef4e8622
wendelad <wendell-pgd@hotmail.com>
2015-05-31 12:12:17 -0300

    funcionalidade de login criada

--- TAGuiando/WebContent/js/scripts.js
+++ TAGuiando/WebContent/js/scripts.js
@@ -31,6 +31,17 @@ function menufixocadastro(){
 	$('#cadastro').animatescroll();
 }
 
+function menufixologin(){
+
+	document.getElementsByClassName('navegacao')[0].setAttribute('style','position:fixed; float:left;width: 260px; height: 700px;');
+	document.getElementById('conteudogeral').style.display="block";
+	document.getElementsByTagName('header')[0].style.display="none";
+	document.getElementById('4').setAttribute('style', 'color:#3c948b');
+	document.getElementById('3').setAttribute('style', 'color:#000');
+	document.getElementById('2').setAttribute('style', 'color:#000');
+	$('#login').animatescroll();
+}
+
 function desfixamenu(){
 	document.getElementsByClassName('navegacao')[0].setAttribute('style','float:left;width: 260px; height: 700px;');
 	document.getElementsByTagName('header')[0].style.display="block";
